a0917a37fda6e9b2b844dfe9058b89c6971e4d4e
Pratiksha Anil Narkhede <97964587+patunarkhede@users.noreply.github.com>
2024-02-25 10:35:23 +0530

    day1.java

--- day1.java
+++ day1.java
@@ -1,3 +1,29 @@
+// Day 1 coding Statement: Write a program to identify if the character is a vowel or consonant.
+
+// Description of the program: 
+
+
+// Take an input character from the user and check whether the given input is a vowel or consonant.
+
+// Input
+// A
+
+// Output
+// Vowel
+
+// Input
+// m
+
+// Output
+// Consonant
+
+// Input
+// 3
+
+// Output
+// Invalid Input
+
+
 import java.util.Scanner;
 
 class day1 {
