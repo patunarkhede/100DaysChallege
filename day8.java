539f3390007e837518521d07c193cda5a5e5150e
Pratiksha Anil Narkhede <97964587+patunarkhede@users.noreply.github.com>
2024-02-28 22:57:41 +0530

    Day 8

---
+++ day8.java
@@ -0,0 +1,40 @@
+// Day 8 coding Statement:  Write a program to find roots of a quadratic equation
+
+// Description
+
+// Get the values of a, b and c (coefficients of quadratic equation) as input from the user and calculate the roots and print as the output.
+
+// Input
+
+// Enter the value of a, b and c : 1 -6 9
+
+// Output
+
+// Roots are equal
+
+// Root 1= root 2 = 3.00
+
+ 
+
+
+
+import java.util.Scanner;
+
+class day8 {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the values of a, b and c :");
+        int n = obj.nextInt();
+        int a = obj.nextInt();
+        int b = obj.nextInt();
+        int c = obj.nextInt();
+        {
+            if (a >= 0) {
+                System.out.println("number");
+
+            }
+        }
+        obj.close();
+    }
+
+}
