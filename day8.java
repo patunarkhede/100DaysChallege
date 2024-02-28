c5ca128559466a21f2436f0f43543861634de841
Pratiksha Anil Narkhede <97964587+patunarkhede@users.noreply.github.com>
2024-02-28 23:11:09 +0530

    day8.java
    Day 8

--- day8.java
+++ day8.java
@@ -1,40 +1,37 @@
-// Day 8 coding Statement:  Write a program to find roots of a quadratic equation
-
-// Description
-
-// Get the values of a, b and c (coefficients of quadratic equation) as input from the user and calculate the roots and print as the output.
-
-// Input
-
-// Enter the value of a, b and c : 1 -6 9
-
-// Output
-
-// Roots are equal
-
-// Root 1= root 2 = 3.00
-
- 
-
-
-
+/*Day 8 coding Statement:  Write a program to find roots of a quadratic equation
+Description:
+Get the values of a, b and c (coefficients of quadratic equation) as input from the user and calculate the roots and print as the output.
+*/
 import java.util.Scanner;
 
-class day8 {
+public class day8 {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
-        System.out.println("Enter the values of a, b and c :");
-        int n = obj.nextInt();
-        int a = obj.nextInt();
-        int b = obj.nextInt();
-        int c = obj.nextInt();
-        {
-            if (a >= 0) {
-                System.out.println("number");
+        System.out.println("Enter the values of a, b and c (coefficients of quadratic equation): ");
+        double a = obj.nextDouble();
+        double b = obj.nextDouble();
+        double c = obj.nextDouble();
+        double root1, root2;
+        double determinant = b * b - 4 * a * c;
+        if (determinant > 0) {
+            System.out.println("The Roots are real and distinct");
+            root1 = (-b + Math.sqrt(determinant) / (2 * a));
+            root2 = (-b - Math.sqrt(determinant) / (2 * a));
+            System.out.println("Root1= " + root1 % .2f + " Root2= " + root2 % .2f);
 
-            }
+        } else if (determinant == 0) {
+            System.out.println("The Roots are real and equal");
+            root1 = root2 = -b / (2 * a);
+            System.out.println("Root1= Root2= " + root1 % .2f);
+
+        } else {
+            System.out.println("The Roots are complex and distinct");
+            double real = -b / (2 * a);
+            double imaginary = Math.sqrt(-determinant) / (2 * a);
+            System.out.format("Root1= %.2f+i%.2f    Root2= %.2f-i%.2f", real, imaginary, real, imaginary);
+
         }
+
         obj.close();
     }
-
 }
